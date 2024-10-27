ab1ab5a4c37990a693318797fd9a055d70c87a1f
Ansel Rognlie <ansel@adadevelopersacademy.org>
2024-10-27 13:52:11 -0700

    adds solutions

--- activity/challenge_1.py
+++ activity/challenge_1.py
@@ -1,2 +1,14 @@
 def uncommon_from_sentences(sentences):
-    pass
+    counts = {}
+    for sentence in sentences:
+        count_words(sentence, counts)
+
+    return find_uncommon(counts)
+
+def count_words(sentence, counts):
+    for word in sentence.split():
+        count = counts.get(word, 0)
+        counts[word] = count + 1
+        
+def find_uncommon(counts):
+    return [word for word, count in counts.items() if count == 1]
--- activity/original.py
+++ activity/original.py
@@ -1,2 +1,13 @@
 def uncommon_from_sentences(s1, s2):
-    pass
+    counts = {}
+    count_words(s1, counts)
+    count_words(s2, counts) 
+    return find_uncommon(counts)
+
+def count_words(sentence, counts):
+    for word in sentence.split():
+        count = counts.get(word, 0)
+        counts[word] = count + 1
+        
+def find_uncommon(counts):
+    return [word for word, count in counts.items() if count == 1]
